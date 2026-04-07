106a34c9dad98e07f8cccd3c351296af031f648e
JackTheCoder303 <JTDubs0909@gmail.com>
2026-04-07 08:49:06 -0500

    Initial Commit

---
+++ CB/CBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBApp.swift
+//  CB
+//
+//  Created by 64012231 on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CBTests/CBTests.swift
@@ -0,0 +1,17 @@
+//
+//  CBTests.swift
+//  CBTests
+//
+//  Created by 64012231 on 4/6/26.
+//
+
+import Testing
+@testable import CB
+
+struct CBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
